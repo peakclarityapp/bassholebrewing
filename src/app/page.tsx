bf61be0a77662022e16c1f7d58f0bed7c7b403d9
Skippy <wayne.goodwin89@gmail.com>
2026-01-31 15:02:04 -0600

    Responsive glob sizes - scale down on mobile/tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -444,6 +444,7 @@ export default function Home() {
         </div>
 
         {/* Floating Paint Globs that transform to data - clustered around logo */}
+        <div className="absolute inset-0 scale-[0.6] md:scale-75 lg:scale-100 origin-center pointer-events-none">
         {[
           // Top area - above the fish
           { color: '#ef4444', x: '35%', y: '8%', size: 10, delay: 0.5, data: '0xFIN' },
@@ -543,6 +544,7 @@ export default function Home() {
             </motion.div>
           </motion.div>
         ))}
+        </div>
 
         <motion.div 
           initial={{ opacity: 0 }}
